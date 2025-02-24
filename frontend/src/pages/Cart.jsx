ca06bcfcfd67940c6f252e2e4ddc56437c152086
nhanquoc-it <caphuunhanquoc2510@gmail.com>
2025-02-24 16:58:50 +0700

    change shipping address

--- frontend/src/pages/Cart.jsx
+++ frontend/src/pages/Cart.jsx
@@ -2,10 +2,13 @@ import { useState } from "react";
 import { useSelector } from "react-redux";
 import { FaTrashAlt } from "react-icons/fa";
 import EmptyCart from "../assets/images/empty-cart.png";
+import Modal from "../components/Modal";
+import ChangeAddress from "../components/ChangeAddress";
 
 const Cart = () => {
 	const cart = useSelector((state) => state.cart);
-	const [address, setAddress] = useState( "mainstreet", "0012");
+	const [address, setAddress] = useState("main street", "0012");
+	const [isModalOpen, setIsModalOpen] = useState(false);
 
 	return (
 		<div className="container mx-auto py-8 min-h-96 px-4 md:px-16 lg:px-24">
@@ -68,7 +71,10 @@ const Cart = () => {
 									shipping to{" "}
 									<span className="text-xs font-bold">{address}</span>
 								</p>
-								<button className="text-blue-500 hover:underline mt-1 ml-2">
+								<button
+									className="text-blue-500 hover:underline mt-1 ml-2"
+									onClick={() => setIsModalOpen(true)}
+								>
 									Change Address
 								</button>
 							</div>
@@ -81,6 +87,13 @@ const Cart = () => {
 							</button>
 						</div>
 					</div>
+
+					<Modal isModalOpen={isModalOpen} setIsModalOpen={setIsModalOpen}>
+						<ChangeAddress
+							setAddress={setAddress}
+							setIsModalOpen={setIsModalOpen}
+						/>
+					</Modal>
 				</div>
 			) : (
 				<div className="flex justify-center">
