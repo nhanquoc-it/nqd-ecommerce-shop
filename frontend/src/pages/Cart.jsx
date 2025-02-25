64806dece1b7bbd99012e5529d1fe61f70416e9e
nhanquoc-it <caphuunhanquoc2510@gmail.com>
2025-02-25 14:48:49 +0700

    increase & decrease quantity

--- frontend/src/pages/Cart.jsx
+++ frontend/src/pages/Cart.jsx
@@ -4,7 +4,11 @@ import { FaTrashAlt } from "react-icons/fa";
 import EmptyCart from "../assets/images/empty-cart.png";
 import Modal from "../components/Modal";
 import ChangeAddress from "../components/ChangeAddress";
-import { removeFromCart } from "../redux/cartSlice";
+import {
+	removeFromCart,
+	decreaseQuantity,
+	increaseQuantity,
+} from "../redux/cartSlice";
 
 const Cart = () => {
 	const cart = useSelector((state) => state.cart);
@@ -46,11 +50,19 @@ const Cart = () => {
 									<div className="flex space-x-12 items-center">
 										<p>$ {product.price}</p>
 										<div className="flex items-center justify-center border">
-											<button className="text-xl font-bold px-1 border-r">
+											<button
+												className="text-xl font-bold px-1 border-r"
+												onClick={() => dispatch(decreaseQuantity(product.id))}
+											>
 												-
 											</button>
 											<p className="text-xl px-2">{product.quantity}</p>
-											<button className="texl-xl px-1 border-l">+</button>
+											<button
+												className="texl-xl px-1 border-l"
+												onClick={() => dispatch(increaseQuantity(product.id))}
+											>
+												+
+											</button>
 										</div>
 										<p>$ {(product.quantity * product.price).toFixed(2)}</p>
 										<button
